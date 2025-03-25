001c0cc9322bc8f87349c2e53ed700ab4cd81089
pradnya1101 <pradnyakolekar1826@gmail.com>
2025-03-25 13:01:18 +0530

    Handled buttons to load and refresh ads

--- app/src/main/java/com/example/testone/MainActivity.kt
+++ app/src/main/java/com/example/testone/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.testone
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.admediationlibrary.AdLoader
@@ -16,6 +17,14 @@ class MainActivity : AppCompatActivity() {
     AdMediationManager.setAdStrategy(AdStrategy.ROUND_ROBIN)
     // Find the ad container
     val adContainer = findViewById<FrameLayout>(R.id.ad_container)
-    AdLoader.loadAd(this, adContainer)
+    val btn_load = findViewById<Button>(R.id.btn_left)
+    val btn_refresh = findViewById<Button>(R.id.btn_right)
+    btn_load.setOnClickListener {
+      AdLoader.loadAd(this, adContainer)
+    }
+    btn_refresh.setOnClickListener {
+      adContainer.removeAllViews()
+      AdLoader.loadAd(this, adContainer)
+    }
   }
 }
